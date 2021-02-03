90ffb2946ab716739f12c0626bcb775a4ac86eab
RobertDragoi <robertdragoi20@gmail.com>
2021-02-03 21:04:45 +0200

    Added routes, auth, middleware and react setup

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB=require('./config/db');
 const app = express();
 const port=process.env.PORT || 5000;
+//Connect to database
+connectDB();
+//Init middleware
+app.use(express.json({extended:false}))
 app.get('/',(req, res)=>res.json({msg:"Welcome to the contact-keeper api", author:"Robert"}));
 
 //Define routes
